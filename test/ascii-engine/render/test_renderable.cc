15fbd52e30f7add018199320414f74f5e710c2cb
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-24 02:24:01 +0000

    Added renderable construction tests

--- test/ascii-engine/render/test_renderable.cc
+++ test/ascii-engine/render/test_renderable.cc
@@ -1,5 +1,21 @@
 #include <gtest/gtest.h>
+#include <ascii-engine/display/renderable.hh>
+#include <string>
 
-TEST(TestStub, OnePlusOneEqualsTwo) {
-  ASSERT_EQ(1 + 1, 2);
+TEST(Renderable, CreateDefault) {
+  Renderable test_renderable;
+
+  ASSERT_EQ(test_renderable.get_x(), 0);
+  ASSERT_EQ(test_renderable.get_y(), 0);
+  ASSERT_EQ(test_renderable.get_str(), "");
+}
+
+TEST(Renderable, CreateWithPositionAndStr) {
+  int x = 5;
+  int y = 6;
+  std::string str = "test";
+
+  ASSERT_EQ(test_renderable.get_x(), x);
+  ASSERT_EQ(test_renderable.get_y(), y);
+  ASSERT_EQ(test_renderable.get_str(), str);
 }
