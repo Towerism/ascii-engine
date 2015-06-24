07a5b7d9d4159dec036c819c2b6242b6397583c2
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-24 20:08:18 +0000

    Add renderer stubs

---
+++ ascii-engine/render/renderer.cc
@@ -0,0 +1,17 @@
+#include "renderer.hh"
+
+Renderer::Renderer() {
+
+}
+
+Renderer::Renderer(int x, int y) {
+
+}
+
+int Renderer::get_width() {
+  return 123;
+}
+
+int Renderer::get_height() {
+  return 321;
+}
---
+++ ascii-engine/render/renderer.hh
@@ -0,0 +1,9 @@
+class Renderer {
+public:
+
+  Renderer();
+  Renderer(int x, int y);
+
+  int get_width();
+  int get_height();
+};
