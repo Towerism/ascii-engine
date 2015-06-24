0325cf66f627857ce40138b83136ad35014db69d
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-24 02:32:24 +0000

    Fixed missing variable declaration

--- test/ascii-engine/render/test_renderable.cc
+++ test/ascii-engine/render/test_renderable.cc
@@ -15,6 +15,8 @@ TEST(Renderable, CreateWithPositionAndStr) {
   int y = 6;
   std::string str = "test";
 
+  Renderable test_renderable(x, y, str);
+
   ASSERT_EQ(test_renderable.get_x(), x);
   ASSERT_EQ(test_renderable.get_y(), y);
   ASSERT_EQ(test_renderable.get_str(), str);
