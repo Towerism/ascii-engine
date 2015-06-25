ba4cb0e60cbcfbc309b24cab97ee563e078bd595
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-25 05:03:46 +0000

    Relink instead of setting env variables

--- ci/install_gcc5.sh
+++ ci/install_gcc5.sh
@@ -6,7 +6,6 @@ sudo apt-get update -qq
 sudo apt-get install g++-5
 sudo apt-get install gcc-5
 
-export CC=gcc-5
-export CXX=g++-5
-
+sudo ln -sf /usr/bin/g++-5 /usr/bin/g++
+sudo ln -sf /usr/bin/gcc-5 /usr/bin/gcc
 sudo ln -sf /usr/bin/gcov-5 /usr/bin/gcov
