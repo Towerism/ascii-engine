6829a9edc89cac84f2ccd6e125000ea8e007bb3b
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-25 05:04:18 +0000

    Don't use sudo in install_gcc.sh

--- ci/install_gcc5.sh
+++ ci/install_gcc5.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update -qq
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt-get update -qq
 
-sudo apt-get install g++-5
-sudo apt-get install gcc-5
+apt-get install g++-5
+apt-get install gcc-5
 
-sudo ln -sf /usr/bin/g++-5 /usr/bin/g++
-sudo ln -sf /usr/bin/gcc-5 /usr/bin/gcc
-sudo ln -sf /usr/bin/gcov-5 /usr/bin/gcov
+ln -sf /usr/bin/g++-5 /usr/bin/g++
+ln -sf /usr/bin/gcc-5 /usr/bin/gcc
+ln -sf /usr/bin/gcov-5 /usr/bin/gcov
