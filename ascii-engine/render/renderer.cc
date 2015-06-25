12aec79536cc994e74f36a6739215b4a0db3bee0
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-25 16:16:09 +0000

    Implemented add

--- ascii-engine/render/renderer.cc
+++ ascii-engine/render/renderer.cc
@@ -4,10 +4,18 @@ Renderer::Renderer(int width, int height) : width(width), height(height) {
 
 }
 
-int Renderer::get_width() {
+void Renderer::add(Renderable* renderable) {
+  renderables.emplace_back(renderable);
+}
+
+int Renderer::get_width() const {
   return width;
 }
 
-int Renderer::get_height() {
+int Renderer::get_height() const {
   return height;
+}
+
+const Renderable* Renderer::get_renderable(int index) const {
+  return renderables[index].get();
 }
--- ascii-engine/render/renderer.hh
+++ ascii-engine/render/renderer.hh
@@ -1,12 +1,23 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include "renderable.hh"
+
 class Renderer {
 public:
 
   Renderer(int width = 0, int height = 0);
 
-  int get_width();
-  int get_height();
+  void add(Renderable* renderable);
+
+  int get_width() const;
+  int get_height() const;
+
+  const Renderable* get_renderable(int index) const;
 
 private:
 
   int width, height;
+  std::vector<std::shared_ptr<Renderable>> renderables;
 };
