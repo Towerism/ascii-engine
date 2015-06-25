73d25686dd72ccf430d2840bb9d76a35aa80e257
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-25 19:53:36 +0000

    Ensure googletest is built with the same flags and compiler as the rest of the build

--- cmake/configure_gtest.cmake
+++ cmake/configure_gtest.cmake
@@ -15,6 +15,7 @@ ExternalProject_Add(
     INSTALL_COMMAND ""
     CMAKE_ARGS
       -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+      -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
 )
 
 # Create a gtest target to be used as a dependency by test programs
