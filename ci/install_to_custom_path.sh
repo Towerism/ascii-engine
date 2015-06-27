d937dcfa5e811ef3c8db17866c32763b8242f64c
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-27 14:11:56 +0000

    Don't install to any special prefix

--- ci/install_to_custom_path.sh
+++ ci/install_to_custom_path.sh
@@ -1,16 +1,11 @@
-if [ ! -z $PREFIX ]; then
-  INSTALL_PREFIX=$PWD
-else
-  INSTALL_PREFIX=$PREFIX
-fi
 
 # install lcov
 git clone https://github.com/linux-test-project/lcov.git
-mkdir -p installed && cd lcov && make install PREFIX=${INSTALL_PREFIX} && cd ..
+mkdir -p installed && cd lcov && make install PREFIX=.. && cd ..
 
 # Create symlinks for new gcc binaries
-ln -s $(which g++-5) ${INSTALL_PREFIX}/usr/bin/g++
-ln -s $(which gcc-5) ${INSTALL_PREFIX}/usr/bin/gcc
-ln -s $(which gcov-5) ${INSTALL_PREFIX}/usr/bin/gcov
+ln -s $(which g++-5) usr/bin/g++
+ln -s $(which gcc-5) usr/bin/gcc
+ln -s $(which gcov-5) usr/bin/gcov
 
 gem install coveralls-lcov
