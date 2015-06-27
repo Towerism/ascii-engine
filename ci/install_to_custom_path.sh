a9a7d8c8e3e9909f05093bbfc8c3161f6b038da2
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-27 17:11:22 +0000

    Update path in profile

--- ci/install_to_custom_path.sh
+++ ci/install_to_custom_path.sh
@@ -1,11 +1,16 @@
+#!/bin/sh
 
 # install lcov
 git clone https://github.com/linux-test-project/lcov.git
 mkdir -p installed && cd lcov && make install PREFIX=.. && cd ..
 
-# Create symlinks for new gcc binaries
+# Create symlinks for gcc-5 binaries
 ln -s $(which g++-5) usr/bin/g++
 ln -s $(which gcc-5) usr/bin/gcc
 ln -s $(which gcov-5) usr/bin/gcov
 
 gem install coveralls-lcov
+
+# Update path in profile
+echo "export PATH=$PWD/usr/bin/:$PATH" >> ~/.profile
+source ~/.profile
