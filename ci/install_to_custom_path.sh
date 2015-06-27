d4241ddc5a6611c2a8b450be77e3307002a8c8da
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-27 14:07:41 +0000

    Fix typos

--- ci/install_to_custom_path.sh
+++ ci/install_to_custom_path.sh
@@ -1,14 +1,16 @@
 if [ ! -z $PREFIX ]; then
-  PREFIX=.
+  INSTALL_PREFIX=$PWD
+else
+  INSTALL_PREFIX=$PREFIX
 fi
 
 # install lcov
 git clone https://github.com/linux-test-project/lcov.git
-mkdir -p installed && cd lcov && make install PREFIX=../installed && cd ..
+mkdir -p installed && cd lcov && make install PREFIX=${INSTALL_PREFIX} && cd ..
 
 # Create symlinks for new gcc binaries
-ln -s $(which g++-5) $(PREFIX)/usr/bin/g++
-ln -s $(which gcc-5) $(PREFIX)/usr/bin/gcc
-ln -s $(which gcov-5) $(PREFIX)/usr/bin/gcov
+ln -s $(which g++-5) ${INSTALL_PREFIX}/usr/bin/g++
+ln -s $(which gcc-5) ${INSTALL_PREFIX}/usr/bin/gcc
+ln -s $(which gcov-5) ${INSTALL_PREFIX}/usr/bin/gcov
 
 gem install coveralls-lcov
