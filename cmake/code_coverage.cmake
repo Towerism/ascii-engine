c4fc6aa74fd7612dd213f105d25a6f2c7450e03d
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-29 05:10:40 +0000

    configure_gtest.cmake was removed

--- cmake/code_coverage.cmake
+++ cmake/code_coverage.cmake
@@ -135,7 +135,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 
     # Capturing lcov counters and generating report
     COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
-    COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'test/*' '/usr/*' --output-file ${_outputname}.info.cleaned
+    COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'test/*' '/usr/*' 'lib/*' --output-file ${_outputname}.info.cleaned
     COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
     COMMAND mv ${_outputname}.info.cleaned ${CMAKE_BINARY_DIR}/${_outputname}/
     COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info
