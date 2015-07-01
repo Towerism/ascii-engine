f766fa82f81221634a904e40750248f0305a2d86
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-01 01:10:43 +0000

    Satisfy PrintLine test

--- ascii-engine/display/terminal_output.cc
+++ ascii-engine/display/terminal_output.cc
@@ -1,10 +1,10 @@
 #include <string>
 #include "terminal_output.hh"
 
-Terminal_output::Terminal_output(Curses_wrapper* curses) {
+Terminal_output::Terminal_output(Curses_wrapper* curses) : curses(curses) {
 
 }
 
 void Terminal_output::print_line(std::string line) {
-
+  curses->print(line.append("\n"));
 }
