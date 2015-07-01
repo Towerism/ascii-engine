3cfbf8950487a0fe932f35500f2186e74ea8e7ef
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-01 01:05:36 +0000

    Add print line test for terminal output

---
+++ test/unit/display/test_terminal_output.cc
@@ -0,0 +1,19 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <string>
+#include <curses_wrapper.hh>
+#include <ascii-engine/display/terminal_output.hh>
+
+class Mock_curses_wrapper : public Curses_wrapper {
+public:
+  MOCK_METHOD1(print, void(std::string));
+};
+
+TEST(TerminalOutput, PrintLine) {
+  Mock_curses_wrapper curses;
+  std::string test_str = "This is a test";
+  EXPECT_CALL(curses, print(test_str + "\n"));
+
+  Terminal_output terminal(&curses);
+  terminal.print_line(test_str);
+}
