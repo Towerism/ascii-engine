0461d0839064d40af266852094c2426b552d1954
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-02 15:15:06 +0000

    Refactor using fixture

--- test/unit/display/test_display.cc
+++ test/unit/display/test_display.cc
@@ -9,7 +9,8 @@
 class Mock_renderer : public Renderer {
 public:
   MOCK_METHOD1(add, void(Renderable*));
-  MOCK_METHOD0(render, std::vector<std::string>());
+  MOCK_METHOD0(render,
+  std::vector<std::string>());
   MOCK_CONST_METHOD0(get_width, int());
   MOCK_CONST_METHOD0(get_height, int());
   MOCK_CONST_METHOD1(get_renderable, Renderable*(int));
@@ -28,35 +29,37 @@ using ::testing::InSequence;
 using ::testing::StrictMock;
 using ::testing::NiceMock;
 
-TEST(Display, UpdateGivenSingleLine) {
+class DisplayTest : public ::testing::Test {
+public:
+  DisplayTest() : display(renderer, terminal) { }
+
   InSequence s;
 
   Mock_renderer* renderer = new Mock_renderer;
+  StrictMock<Mock_terminal>* terminal = new StrictMock<Mock_terminal>;
+
+  Display display;
+};
+
+TEST_F(DisplayTest, UpdateGivenSingleLine) {
   std::vector<std::string> test_rendered = { "test line" };
   EXPECT_CALL(*renderer, render())
     .WillOnce(Return(test_rendered));
 
-  StrictMock<Mock_terminal>* terminal = new StrictMock<Mock_terminal>;
   EXPECT_CALL(*terminal, print(test_rendered[0] + "\n"));
   EXPECT_CALL(*terminal, hard_refresh());
 
-  Display display(renderer, terminal);
   display.update();
 }
 
-TEST(Display, UpdateGivenTwoUnidenticalLines) {
-  InSequence s;
-
-  Mock_renderer* renderer = new Mock_renderer;
+TEST_F(DisplayTest, UpdateGivenTwoUnidenticalLines) {
   std::vector<std::string> test_rendered = { "test line 1", "test line 2" };
   EXPECT_CALL(*renderer, render())
     .WillOnce(Return(test_rendered));
 
-  StrictMock<Mock_terminal>* terminal = new StrictMock<Mock_terminal>;
   EXPECT_CALL(*terminal, print(test_rendered[0] + "\n"));
   EXPECT_CALL(*terminal, print(test_rendered[1] + "\n"));
   EXPECT_CALL(*terminal, hard_refresh());
 
-  Display display(renderer, terminal);
   display.update();
 }
